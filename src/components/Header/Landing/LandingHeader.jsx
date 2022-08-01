a2b779797db4ffac8e0a02bdce35428891f68e10
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-08-01 13:40:29 +0800

    Create landing header

--- src/components/Header/Landing/LandingHeader.jsx
+++ src/components/Header/Landing/LandingHeader.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Group, Button, Text, Divider } from '@mantine/core';
+import { BuildingBank } from 'tabler-icons-react';
 
 const LandingHeader = () => {
-  return <div>LandingHeader</div>;
+  return (
+    <>
+      <Group position="apart" className="landing-header-container">
+        <Group spacing={6} className="cursor-pointer">
+          <BuildingBank size={24} />
+          <Text className="bold-text">Banking</Text>
+        </Group>
+        <Button variant="outline">Sign in</Button>
+      </Group>
+      <Divider my="sm" />
+    </>
+  );
 };
 
 export default LandingHeader;
