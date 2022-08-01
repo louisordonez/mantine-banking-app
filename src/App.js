86d31f2a9e6191b1d0599b83ef251fc845c46191
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-08-01 13:25:17 +0800

    Add styles base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
+import Home from './pages/Landing/Home/Home';
+import Login from './pages/Landing/Login/Login';
+import SignUp from './pages/Landing/SignUp/SignUp';
 
 function App() {
   return (
---
+++ src/pages/Landing/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingHeader from '../../../components/Header/Landing/LandingHeader';
+
+const Home = () => {
+  return (
+    <>
+      <LandingHeader />
+      <div>Home</div>
+    </>
+  );
+};
+
+export default Home;
