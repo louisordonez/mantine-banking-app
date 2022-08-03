36a4859c4ace70d8f97d860fe0220464021389ab
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-08-03 11:47:48 +0800

    Assign userList to local storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Landing/Home/Home';
 import Login from './pages/Landing/Login/Login';
 import SignUp from './pages/Landing/SignUp/SignUp';
 import Client from './pages/Client/Client';
 import Error404 from './pages/Error/Error404/Error404';
+import {
+  assignLocalStorageItem,
+  getLocalStorageItem,
+} from './services/utilities/localStorage';
+import { USER_LIST } from './services/constants/userList';
 
 function App() {
+  useEffect(() => {
+    const userListLocalStorage = getLocalStorageItem('userList');
+
+    if (userListLocalStorage === null) {
+      assignLocalStorageItem('userList', USER_LIST);
+    }
+  }, []);
+
   return (
     <>
       <Routes>
