1e824a6435ca31ffc07e3b10d6e81f61d9e0667a
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-08-14 13:20:52 +0800

    Update expense list

--- src/services/constants/expenseList.js
+++ src/services/constants/expenseList.js
@@ -11,13 +11,13 @@ export const EXEPENSE_LIST = [
     accountNumber: 1560222970000,
     item: 'Electricity',
     amount: 4000.0,
-    timestamp: '2022-08-14T01:27:01.980Z',
+    timestamp: '2022-08-14T01:28:02.980Z',
   },
   {
     id: 1660368424,
     accountNumber: 1560222970001,
     item: 'Water',
     amount: 2000.0,
-    timestamp: '2022-08-14T01:27:01.980Z',
+    timestamp: '2022-08-14T01:29:03.980Z',
   },
 ];
